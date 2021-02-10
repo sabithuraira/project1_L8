ce70c94d736709318519773206c39d54599391a1
Sabit Huraira <sabitzhabit@gmail.com>
2021-02-10 17:48:52 +0700

    membuat controller dg command & route controller dg resource

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PhotoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,3 +24,19 @@ Route::get('/halo', function () {
 
 Route::get('/home/halo', [HomeController::class, 'halo']);
 Route::get('/home/haloname/{name}', [HomeController::class, 'haloWithName']);
+
+//MENDEFINISIKAN ROUTE BERDASARKAN SEMUA FUNGSI DEFAULT LARAVEL
+//PADA SUATU CONTROLLER
+Route::resource('photos', PhotoController::class);
+
+//JIKA HANYA INGIN MENGAKTIFKAN BEBERAPA FUNGSI PADA CONTROLLER
+//GUNAKAN 'only' PADA ROUTES
+// Route::resource('photos', PhotoController::class)->only([
+//     'index', 'show'
+// ]);
+
+//JIKA HANYA INGIN MEMBUANG BEBERAPA FUNGSI PADA CONTROLLER
+//GUNAKAN 'except' PADA ROUTES
+// Route::resource('photos', PhotoController::class)->except([
+//     'create', 'store', 'update', 'destroy'
+// ]); 
